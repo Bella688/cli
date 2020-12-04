83497c4e0c638023722addf97643b07de8d77be6
vilmibm <vilmibm@neongrid.space>
2020-12-04 12:54:28 -0800

    WIP: start on list tests

---
+++ pkg/cmd/secret/list/list_test.go
@@ -0,0 +1,71 @@
+package list
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/cli/cli/pkg/cmdutil"
+	"github.com/cli/cli/pkg/iostreams"
+	"github.com/google/shlex"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_NewCmdList(t *testing.T) {
+	tests := []struct {
+		name  string
+		cli   string
+		wants ListOptions
+	}{
+		{
+			name: "repo",
+			cli:  "",
+			wants: ListOptions{
+				OrgName: "",
+			},
+		},
+		{
+			name: "implicit org",
+			cli:  "--org",
+			wants: ListOptions{
+				OrgName: "@owner",
+			},
+		},
+		{
+			name: "explicit org",
+			cli:  "--org=UmbrellaCorporation",
+			wants: ListOptions{
+				OrgName: "UmbrellaCorporation",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			io, _, _, _ := iostreams.Test()
+			f := &cmdutil.Factory{
+				IOStreams: io,
+			}
+
+			argv, err := shlex.Split(tt.cli)
+			assert.NoError(t, err)
+
+			var gotOpts *ListOptions
+			cmd := NewCmdList(f, func(opts *ListOptions) error {
+				gotOpts = opts
+				return nil
+			})
+			cmd.SetArgs(argv)
+			cmd.SetIn(&bytes.Buffer{})
+			cmd.SetOut(&bytes.Buffer{})
+			cmd.SetErr(&bytes.Buffer{})
+
+			_, err = cmd.ExecuteC()
+			assert.NoError(t, err)
+
+			assert.Equal(t, tt.wants.OrgName, gotOpts.OrgName)
+
+		})
+	}
+}
+
+// TODO run tests
